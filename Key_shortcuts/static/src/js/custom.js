f20cd19246194e789239bea4926297cc1a83c3f0
hiworth <hiworth.hiworth@gmail.com>
2022-11-16 15:49:11 +0530

    homeo f2 shortkey form create

--- Key_shortcuts/static/src/js/custom.js
+++ Key_shortcuts/static/src/js/custom.js
@@ -1,10 +1,83 @@
 openerp.Key_shortcuts = function (jQuery) {
-    $(document).bind('keydown', 'f2', function assets() {
-        window.location.href = window.location.protocol + '//' + window.location.host + '/' + 'web?debug=1#view_type=form&model=account.invoice&menu_id=441&action=395';
-    setTimeout(function(){
-    $('button.oe_button.oe_form_button_create').click();
-}, 4000)
+//    $(document).bind('keydown', 'f2', function assets() {
+////        window.location.href = window.location.protocol + '//' + window.location.host + '/' + 'web?debug=1#view_type=form&model=account.invoice&menu_id=441&action=395';
+//        window.location.href = window.location.protocol + '//' + window.location.host + '/' + 'web?debug=1#view_type=form&model=account.invoice&action=395';
+//    setTimeout(function(){
+//    $('button.oe_button.oe_form_button_create').click();
+//}, 4000)
+//    });
+
+//openerp.Key_shortcuts = function (jQuery) {
+//    $(document).bind('keydown', 'f2', function assets() {
+//
+//    $.when(function(){
+//
+//
+//      window.location.assign("https://www.w3schools.com")
+//
+////            window.location.href = window.location.protocol + '//' + window.location.host + '/' + 'web?debug=1#view_type=form&model=account.invoice&menu_id=441&action=395';
+//    }).done(function(){
+//            $('button.oe_button.oe_form_button_create').click();
+//
+//    });
+//
+
+
+
+
+
+////
+//$.ctrl = function(key, callback, args) {
+//    $(document).keydown(function(e) {
+//        if(!args) args=[]; // IE barks when args is null
+//        console.log(e.keyCode)
+//        if((e.keyCode == key.charCodeAt(0) || e.keyCode == key) && e.ctrlKey) {
+//            callback.apply(this, args);
+//            return false;
+//        }
+//    });
+//};
+//
+//
+$.shortcut = function(key, callback, args) {
+    $(document).keydown(function(e) {
+        if(!args) args=[]; // IE barks when args is null
+        console.log(e.keyCode)
+        if((e.keyCode == key.charCodeAt(0) || e.keyCode == key)) {
+            callback.apply(this, args);
+            return false;
+        }
     });
+};
+//console.log('1');
+//    $.ctrl('13', function() {
+//        $('.oe_form_button_create').each(function() {
+//            if($(this).parents('div:hidden').length == 0){
+//                $(this).trigger('click');
+//            }
+//        });
+//        $('.oe_list_add').each(function() {
+//            if($(this).parents('div:hidden').length == 0){
+//                $(this).trigger('click');
+//            }
+//        });
+//    });
+//console.log('2');
+
+    $.shortcut('113', function() {
+	$('.oe_form_button_create').each(function() {
+		if($(this).parents('div:hidden').length == 0){
+			$(this).trigger('click');
+		}
+	});
+	$('.oe_list_add').each(function() {
+            if($(this).parents('div:hidden').length == 0){
+                $(this).trigger('click');
+            }
+        });
+});
+//console.log('f3');
+
 //    ROBIN'S CODE
     $( document ).ready(function() {
     //For adding class to html tag
